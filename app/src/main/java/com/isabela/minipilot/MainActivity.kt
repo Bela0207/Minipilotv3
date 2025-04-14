52357d7eae8c685378c1a809d30719d5b75abf91
20222TIOT0357 <belacarvalhogodoy@gmail.com>
2025-04-14 16:31:45 -0300

    Initial commit

---
+++ app/src/main/java/com/isabela/minipilot/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.isabela.minipilot
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Loopper.getMainLooper()).postDelayed ({
+            val intent = Intent(this, Telainicial::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
+
